0d63e2e2d0976388af9b791f5f4161730f295bb1
John Claro <jkrclaro@gmail.com>
2019-11-13 12:31:02 +0000

    Remove thumbnail key

---
+++ utils.py
@@ -0,0 +1,25 @@
+import json
+
+
+def print_format(data):
+    print(
+        '{"speakers": ["%s"], "title": "%s", "video_url": "%s", "convention": "%s", "uploaded_at": "%s", "channel_url": "%s"},' % (
+            data['speakers'][0],
+            data['title'],
+            data['video_url'],
+            data['convention'],
+            data['uploaded_at'],
+            data['channel_url']
+        )
+    )
+
+
+def main():
+    with open('src/talks.json') as json_file:
+        talks = json.loads(json_file.read())['data']
+        for talk in talks:
+            print_format(talk)
+
+
+if __name__ == '__main__':
+    main()
