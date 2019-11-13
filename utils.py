e23975506915ceaba685ded8822e7d131c25f2e8
John Claro <jkrclaro@gmail.com>
2019-11-13 14:30:58 +0000

    Add more categories to be updated

--- utils.py
+++ utils.py
@@ -29,35 +29,58 @@ def main():
         for talk in talks:
             title = talk['title'].lower()
 
-            for category in ('deep learning', 'machine learning', 'data', 'computational', 'statistics',):
+            # This should be at the top because python is such a used word.
+            for category in (
+                'community', 'pep', 'data structure', 'algorithms', 'gil',
+                'pip', 'asyncio', 'python2', 'python3', 'python', 'type',
+            ):
                 if category in title:
-                    talk['category']['title'] = 'Category: Data'
-                    talk['category']['badge'] = 'info'
+                    talk['category']['title'] = 'Category: Core'
+                    talk['category']['badge'] = 'python'
 
-            for category in ('exception', 'hacking', 'security',):
+            for category in (
+                'exception', 'hacking', 'security', 'threat', 'testing', 'bugs',
+            ):
                 if category in title:
                     talk['category']['title'] = 'Category: Security'
                     talk['category']['badge'] = 'danger'
 
-            for category in ('keynote', 'wrap up', 'lightning talk', 'remarks', 'thank you',):
+            for category in (
+                'keynote', 'wrap up', 'lightning talk', 'remarks', 'thank you',
+                'panel discussion',
+            ):
                 if category in title:
                     talk['category']['title'] = 'Category: Others'
                     talk['category']['badge'] = 'light'
 
-            for category in ('contributor', 'blog', 'leader', 'maintaining',):
+            for category in (
+                'contributor', 'blog', 'leader', 'maintaining',
+                'documentation', 'code review', 'readability',
+                'open source license', 'open source self-care',
+                'open source for newcomers', 'participating in open source',
+                'novice', 'n00bs',
+            ):
                 if category in title:
                     talk['category']['title'] = 'Category: Career'
                     talk['category']['badge'] = 'success'
 
-            for category in ('server', 'web', 'django', 'flask', 'ORM', 'networking',):
+            for category in (
+                'server', 'web', 'django', 'flask', 'ORM', 'networking',
+                'protocol', 'container', 'api', 'sqlalchemy', 'alembic',
+                'instagram', 'http',
+            ):
                 if category in title:
                     talk['category']['title'] = 'Category: Web'
                     talk['category']['badge'] = 'primary'
 
-            for category in ('community', 'pep',):
+            for category in (
+                'deep learning', 'machine learning', 'data', 'computational',
+                'statistics', 'pyspark', 'network analysis', 'jupyter',
+                'ipython', 'pandas', 'diagnostic', 'hadoop',
+            ):
                 if category in title:
-                    talk['category']['title'] = 'Category: Core'
-                    talk['category']['badge'] = 'python'
+                    talk['category']['title'] = 'Category: Data'
+                    talk['category']['badge'] = 'info'
 
 
     with open(talks_filepath, 'w') as json_file:
