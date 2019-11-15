d969c57860b6729e99531a4b05ee4dbc2be782f8
John Claro <jkrclaro@gmail.com>
2019-11-15 13:57:24 +0000

    Dynamically update categories via utils.py

--- utils.py
+++ utils.py
@@ -2,70 +2,29 @@ import json
 
 
 def main():
-    talks_filepath = 'src/talks.json'
-    with open(talks_filepath) as json_file:
+    talks_path = 'src/talks.json'
+    categories_path = 'src/categories.json'
+
+    with open(talks_path) as json_file:
         talks = json.loads(json_file.read())['data']
-        for talk in talks:
-            title = talk['title'].lower()
 
-            # This should be at the top because `python` is such a used word.
-            for category in (
-                'community', 'pep', 'data structure', 'algorithms', 'gil',
-                'pip', 'asyncio', 'python2', 'python3', 'python', 'type',
-                'descriptor', 'testing', 'bugs',
-            ):
-                if category in title:
-                    talk['category']['title'] = 'Programming'
-                    talk['category']['badge'] = 'python'
+        with open(categories_path) as categories_file:
+            categories = json.loads(categories_file.read())['data']
 
-            for category in (
-                'exception', 'hacking', 'security', 'threat',
-            ):
-                if category in title:
-                    talk['category']['title'] = 'Security'
-                    talk['category']['badge'] = 'danger'
+            for talk in talks:
+                title = talk['title'].lower()
 
-            for category in (
-                'contributor', 'blog', 'leader', 'maintaining',
-                'documentation', 'code review', 'readability',
-                'open source license', 'open source self-care',
-                'open source for newcomers', 'participating in open source',
-                'novice', 'n00bs', 'interview',
-            ):
-                if category in title:
-                    talk['category']['title'] = 'Career'
-                    talk['category']['badge'] = 'success'
+                for category in categories:
+                    for keyword in category['keywords']:
+                        if keyword in title:
+                            talk['category']['title'] = category['title']
+                            talk['category']['badge'] = category['badge']
 
-            for category in (
-                'server', 'web', 'django', 'flask', 'ORM', 'networking',
-                'protocol', 'container', 'api', 'sqlalchemy', 'alembic',
-                'instagram', 'http', 'localization',
-            ):
-                if category in title:
-                    talk['category']['title'] = 'Web'
-                    talk['category']['badge'] = 'primary'
-
-            for category in (
-                'deep learning', 'machine learning', 'data', 'computational',
-                'statistics', 'pyspark', 'network analysis', 'jupyter',
-                'ipython', 'pandas', 'diagnostic', 'hadoop', 'numpy',
-            ):
-                if category in title:
-                    talk['category']['title'] = 'Data'
-                    talk['category']['badge'] = 'info'
-
-            for category in (
-                'keynote', 'wrap up', 'lightning talk', 'remarks', 'thank you',
-                'panel discussion', 'music',
-            ):
-                if category in title:
-                    talk['category']['title'] = 'Others'
-                    talk['category']['badge'] = 'light'
-
-
-    with open(talks_filepath, 'w') as json_file:
+    with open(talks_path, 'w') as json_file:
         json.dump({'data': talks}, json_file, indent=2)
 
 
 if __name__ == '__main__':
+    print('Updating talks.json...')
     main()
+    print('Updated talks.json!')
